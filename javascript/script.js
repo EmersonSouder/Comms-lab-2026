8f0c1541e2c7885ec161040b96f678f584a738a7
EmersonSouder <eas10124@nyu.edu>
2026-03-25 22:34:58 +0800

    tutorial

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+let name1 = "emerson";
+function greet(){
+    let language3 = "javascript";
+    console.log("The button was clicked.");
+    alert("Hello!");
+    console.log("This function finished.");
+}
+
+function myFunction({
+    console.log(language3+"is fun.")
+})
+
+function changeHeadline()[
+    console.log("function runs. changing headline");
+    document.querySelector("h1").innerText = "New title!";
+]
